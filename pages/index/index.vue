531d4c3b7485a51d422804127930f0aeecb327c4
ChangDh <1392238525@qq.com>
2024-11-24 00:44:15 +0800

    新增备案信息

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -8,8 +8,8 @@
 				<uni-col :sm="24" :md="12">
 					<uni-group title="抽卡模拟" mode="card">
 						<view class="group-item" @click="goPath('/pages/godsNotIncluded/index')"><uni-tag text="周年庆模式"
-								type="primary" /></view>	
-								<view class="group-item" @click="goPath('/pages/guarantees60/index')"><uni-tag text="60保底模式"
+								type="primary" /></view>
+						<view class="group-item" @click="goPath('/pages/guarantees60/index')"><uni-tag text="60保底模式"
 								type="primary" /></view>
 						<view class="group-item" @click="goPath('/pages/up3/index')"><uni-tag text="3up模式"
 								type="primary" /></view>
@@ -18,15 +18,15 @@
 						<view class="group-item" @click="goPath('/pages/getSpeed18/index')"><uni-tag text="出头模式"
 								type="primary" /></view>
 					</uni-group>
-				<!-- 	<uni-group title="藏宝阁相关" mode="card">
+					<!-- 	<uni-group title="藏宝阁相关" mode="card">
 						<view class="group-item" @click="goPath('/pages/account-details/search-cbg')"><uni-tag text="账号御魂分析"
 								type="primary" /></view>	
 					</uni-group> -->
 				</uni-col>
 				<uni-col :sm="24" :md="12">
 					<uni-group title="图鉴" mode="card">
-						<view class="group-item" @click="goPath('/pages/illustrated/illustrated')"><uni-tag text="查看式神图鉴"
-								type="primary" /></view>
+						<view class="group-item" @click="goPath('/pages/illustrated/illustrated')"><uni-tag
+								text="查看式神图鉴" type="primary" /></view>
 					</uni-group>
 					<uni-group title="萌新养成推荐" mode="card">
 						<view class="group-item" @click="goPath('/pages/godsRecommend/godsRecommend')"><uni-tag
@@ -41,6 +41,11 @@
 			</uni-row>
 		</view>
 	</view>
+	<view class="beian">
+		<image src="./beian-icon.png" mode=""></image>
+		<a href="https://beian.mps.gov.cn/#/query/webSearch" rel="noreferrer" target="_blank">鲁公网安备37050202371540</a><a
+			href="https://beian.miit.gov.cn/" rel="noreferrer" target="_blank">鲁ICP备2022008257号</a>
+	</view>
 </template>
 
 <script setup lang="ts">
@@ -49,12 +54,39 @@
 			url,
 		})
 	}
+	
 </script>
 <style lang="scss" scoped>
-	.page{
+	.beian {
+		width: 100%;
+		line-height: 50px;
+		text-align: center;
+		color: #fff;
+		font-size: 12px;
+		background-color: #f2f2f2;
+		background-color: #043e5a;
+
+		image {
+			width: 15px;
+			height: 15px;
+			margin-right: 5px;
+			vertical-align: middle;
+		}
+
+		a {
+			color: #fff;
+			text-decoration: none;
+			margin-right: 20px;
+		}
+	}
+
+	.page {
 		max-width: 800px;
 		margin: auto;
+		overflow: hidden;
+		min-height: calc(100vh - 50px);
 	}
+
 	.title {
 		margin-top: 50rpx;
 	}
