5272473f0489dcbd62809facf163c20c0a08f7f7
changdh <cdh8749@163.com>
2024-12-24 10:13:11 +0800

    清除了备案信息

---
+++ components/beian.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="beian">
+		<image src="./beian-icon.png" mode=""></image>
+		<a href="https://beian.mps.gov.cn/#/query/webSearch" rel="noreferrer" target="_blank">鲁公网安备37050202371540</a><a
+			href="https://beian.miit.gov.cn/" rel="noreferrer" target="_blank">鲁ICP备2022008257号</a>
+	</view>
+</template>
+
+<script setup>
+	
+</script>
+
+<style lang="scss" scoped>
+	.beian {
+		width: 100%;
+		line-height: 50px;
+		text-align: center;
+		color: #fff;
+		font-size: 12px;
+		background-color: #f2f2f2;
+		background-color: #043e5a;
+
+		image {
+			width: 15px;
+			height: 15px;
+			margin-right: 5px;
+			vertical-align: middle;
+		}
+
+		a {
+			color: #fff;
+			text-decoration: none;
+			margin-right: 20px;
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -42,14 +42,11 @@
 			</uni-row>
 		</view>
 	</view>
-	<view class="beian">
-		<image src="./beian-icon.png" mode=""></image>
-		<a href="https://beian.mps.gov.cn/#/query/webSearch" rel="noreferrer" target="_blank">鲁公网安备37050202371540</a><a
-			href="https://beian.miit.gov.cn/" rel="noreferrer" target="_blank">鲁ICP备2022008257号</a>
-	</view>
+	<beian />
 </template>
 
 <script setup lang="ts">
+	import beian from '@/components/beian'
 	import { version } from '@/config'
 	const goPath = (url : string) => {
 		uni.navigateTo({
@@ -58,29 +55,6 @@
 	}
 </script>
 <style lang="scss" scoped>
-	.beian {
-		width: 100%;
-		line-height: 50px;
-		text-align: center;
-		color: #fff;
-		font-size: 12px;
-		background-color: #f2f2f2;
-		background-color: #043e5a;
-
-		image {
-			width: 15px;
-			height: 15px;
-			margin-right: 5px;
-			vertical-align: middle;
-		}
-
-		a {
-			color: #fff;
-			text-decoration: none;
-			margin-right: 20px;
-		}
-	}
-
 	.page {
 		max-width: 800px;
 		margin: auto;
